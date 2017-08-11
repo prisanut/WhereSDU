6d20065c0844510f50aecd6118a4747f725c59c3
prisana <prisanut@hotmail.com>
2017-08-11 13:57:56 +0700

    GetLatLng

--- app/src/main/java/sdu/mutchima/prisana/wheresdu/ServiceActivity.java
+++ app/src/main/java/sdu/mutchima/prisana/wheresdu/ServiceActivity.java
@@ -1,7 +1,17 @@
 package sdu.mutchima.prisana.wheresdu;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,6 +25,10 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
 {
 
     private GoogleMap mMap;
+    private double latADouble = 0, lngADouble = 0;
+    private LocationManager locationManager;
+    private Criteria criteria;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,8 +41,136 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
         // Show Title
         showTitle();
 
+        // Off Controller
+        offController();
+
+        // Setup for Get Location
+        setupForGetLocation();
+
 
     } // Main Method
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        myGetLocation();
+        
+    }
+
+    private void myGetLocation()
+    {
+        locationManager.removeUpdates(locationListener);
+        // For Network
+        Location netWorkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);
+        if (netWorkLocation != null)
+        {
+            latADouble = netWorkLocation.getLatitude();
+            lngADouble = netWorkLocation.getLongitude();
+
+        }
+
+
+        // For GPS
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null)
+        {
+            latADouble = gpsLocation.getLatitude();
+            lngADouble = gpsLocation.getLongitude();
+
+        }
+
+        Log.d("11AugV1", "Lat ==> " + latADouble);
+        Log.d("11AugV1", "Lng ==> " + lngADouble);
+
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+
+        locationManager.removeUpdates(locationListener);
+
+    }
+
+    public Location myFindLocation(String strProvider)
+    {
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider))
+        {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return null;
+            }
+            locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+
+        } //if
+
+        return location;
+    }
+
+    public LocationListener locationListener = new LocationListener()
+    {
+        @Override
+        public void onLocationChanged(Location location)
+        {
+            latADouble = location.getLatitude();
+            lngADouble = location.getLongitude();
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle)
+        {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s)
+        {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s)
+        {
+
+        }
+    };
+
+    private void setupForGetLocation()
+    {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+    }
+
+    private void offController()
+    {
+        ImageView imageView = findViewById(R.id.imvOff);
+        imageView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                finish();
+            }
+        });
+    }
 
     private void showTitle()
     {
@@ -50,10 +192,20 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
     {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // Check lat, lng != 0
+        while (latADouble == 0)
+        {
+            myGetLocation();
+        } // while
+
+
+        LatLng latLng = new LatLng(latADouble, lngADouble);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+
+
+
+
     } // on MapReady
 
-} // Main
+} // Main Class
